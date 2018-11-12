8d13322b64fdfeb1ba4c571fa5129dc66a248dcf
胡书康 <hushukang@gmail.com>
2018-11-12 11:04:51 +0900

    Update Action.java

--- 计算器/Action.java
+++ 计算器/Action.java
@@ -2,9 +2,10 @@ package pack01;
 
 public class Action {
     public static void main(String[] args) {
-        String enterLine = "((11 + 1))";
+        String enterLine = "(((1 + 6) * 3) + (15 - 5)) * 10";
         CheckUtil.check(enterLine);
         String result = Calculator.invoke(enterLine);
-        System.out.println(result);
+        System.out.println("算式: " + enterLine);
+        System.out.println("結果: " + result);
     }
 }
